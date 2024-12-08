0580800fc1b6bdb51bf3ece367ac4242c83ddba6
Vinay Sethia <sethiavinay4u@gmail.com>
2024-12-08 09:01:19 +0530

    code changes only for readability

--- src/TodoApp.API/Behaviors/LoggingBehavior.cs
+++ src/TodoApp.API/Behaviors/LoggingBehavior.cs
@@ -1,22 +1,18 @@
 ﻿namespace TodoApp.API.Behaviors;
 
-internal sealed class LoggingBehavior<TRequest, TResponse>(
-    ILogger<LoggingBehavior<TRequest, TResponse>> logger)
-    : IPipelineBehavior<TRequest, TResponse>
+internal sealed class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger) : IPipelineBehavior<TRequest, TResponse>
     where TRequest : notnull, IRequest<TResponse>
     where TResponse : notnull
 {
-    public async Task<TResponse> Handle(TRequest request,
-        RequestHandlerDelegate<TResponse> next,
-        CancellationToken cancellationToken)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         logger.LogInformation("[START] Handle Request={Request} - Response={Response} - RequestData={RequestData}",
-            typeof(TRequest).Name, typeof(TResponse).Name, request);
+                              typeof(TRequest).Name, typeof(TResponse).Name, request);
 
         var response = await next();
 
         logger.LogInformation("[END] Handled {Request} with {Response}",
-            typeof(TRequest).Name, typeof(TResponse).Name);
+                              typeof(TRequest).Name, typeof(TResponse).Name);
 
         return response;
     }
--- src/TodoApp.API/Behaviors/ValidationBehaviour.cs
+++ src/TodoApp.API/Behaviors/ValidationBehaviour.cs
@@ -1,19 +1,13 @@
 ﻿namespace TodoApp.API.Behaviors;
 
-public class ValidationBehavior<TRequest, TResponse>
-    (IEnumerable<IValidator<TRequest>> validators)
-
-    : IPipelineBehavior<TRequest, TResponse>
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators) : IPipelineBehavior<TRequest, TResponse>
     where TRequest : ICommand<TResponse>
 {
-    public async Task<TResponse> Handle(TRequest request,
-        RequestHandlerDelegate<TResponse> next, 
-        CancellationToken cancellationToken)
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
     {
         var context = new ValidationContext<TRequest>(request);
 
-        var validationResult =
-            await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+        var validationResult await Task.WhenAll(validators.Select(v => v.ValidateAsync(context, cancellationToken)));
 
         var failures = validationResult
             .Where(v => v.Errors.Any())
